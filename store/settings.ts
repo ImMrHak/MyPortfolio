00394c32d577dff0078c6dcc4e5e440f3112ddfb
ImMrHak <hakkoumohamed23@gmail.com>
2025-01-22 22:30:44 +0100

    Fixing toggleAutoScroll

--- store/settings.ts
+++ store/settings.ts
@@ -3,9 +3,11 @@ import { create } from 'zustand'
 interface SettingsState {
   autoScroll: boolean
   setAutoScroll: (autoScroll: boolean) => void
+  toggleAutoScroll: () => void
 }
 
 export const useSettings = create<SettingsState>((set) => ({
   autoScroll: true,
   setAutoScroll: (autoScroll) => set({ autoScroll }),
+  toggleAutoScroll: () => set((state) => ({ autoScroll: !state.autoScroll })),
 }))
